48f402e18e3ad1044314e393490fe002036bac53
AdamCranial <adam@hofh.org.uk>
2022-02-10 16:48:54 +0000

    update fhir3 ro r4 conversion

--- src/main/java/uk/org/nhs/lidoe/medications/HackathonUsercase/MedicationController.java
+++ src/main/java/uk/org/nhs/lidoe/medications/HackathonUsercase/MedicationController.java
@@ -7,6 +7,8 @@ import javax.naming.NameNotFoundException;
 import javax.servlet.http.HttpServletRequest;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.ibm.fhir.exception.FHIRException;
+import com.ibm.fhir.model.generator.FHIRAbstractGenerator;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -45,7 +47,12 @@ import ca.uhn.hl7v2.app.Connection;
 import ca.uhn.hl7v2.app.Initiator;
 
 import io.github.linuxforhealth.hl7.HL7ToFHIRConverter;
- 
+
+import org.hl7.fhir.convertors.conv30_40.Bundle30_40;
+import org.hl7.fhir.r4.model.Bundle;
+
+
+import ca.uhn.fhir.context.FhirContext;
 
 @RestController
 public class MedicationController {
@@ -55,7 +62,73 @@ public class MedicationController {
     private static final String HL7_MLLP_HOST = "localhost";// change this to whatever your port number is
     private static HapiContext context = new DefaultHapiContext();
 
-    // take a HL7 message and send it on to a local MLLP server.
+    // take a FHIR message and verify it 
+    //  note we cannot parse the HL7 message directly in the @RequestBody because it is not json/xml
+    // so hence the String and parsing inside of the function based on HAPI HL7 system.
+    
+    @RequestMapping(value="/fhir/**",
+                  headers="Accept=*/*",
+                  method = { RequestMethod.POST },
+                  consumes = {MediaType.APPLICATION_JSON_VALUE},
+                  produces = {MediaType.APPLICATION_JSON_VALUE} )
+    @ResponseBody
+    public String processFHIRRequest(
+        @RequestParam Map<String, String> reqParam,
+        @RequestHeader Map<String, String> reqHeaders, 
+        @RequestBody org.hl7.fhir.r4.model.Bundle fhirInput,
+        HttpServletRequest request) 
+            throws IOException, NameNotFoundException {
+                try {
+                    //FhirContext ctx = FhirContext.forDstu3();
+                    
+                     FhirContext ctx= FhirContext.forR4();
+                    String json = ctx.newJsonParser().encodeResourceToString(fhirInput);    
+                    return json;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return "whoops" ;
+        }  
+       
+
+    @RequestMapping(value="/fhir3/**",
+        headers="Accept=*/*",
+        method = { RequestMethod.POST },
+        consumes = {MediaType.APPLICATION_JSON_VALUE},
+        produces = {MediaType.APPLICATION_JSON_VALUE} )
+    @ResponseBody
+    public String processFHIR3Request(
+    @RequestParam Map<String, String> reqParam,
+    @RequestHeader Map<String, String> reqHeaders, 
+    @RequestBody org.hl7.fhir.dstu3.model.Bundle fhirInput3,
+    HttpServletRequest request) 
+    throws IOException, NameNotFoundException {
+        try {
+          //FhirContext ctx = FhirContext.forDstu3();
+          
+          FhirContext ctx= FhirContext.forR4();
+
+          // Convert the incoming R3 bundle probably from GP Connect to R4.
+         org.hl7.fhir.r4.model.Bundle b4 = Bundle30_40.convertBundle(fhirInput3);
+        
+          String json = ctx.newJsonParser().encodeResourceToString(b4);    
+
+
+          return json;
+      } catch (Exception e) {
+          e.printStackTrace();
+      }
+      return "whoops" ;
+}     
+        // public String toJSON3() {
+        
+        //     FhirContext ctx3 = FhirContext.forDstu3();
+        //     org.hl7.fhir.dstu3.model.Bundle b3 = Bundle10_30.convertBundle(bMaternity);
+    
+        //     return ctx3.newJsonParser().encodeResourceToString(b3);
+        // }
+
+// take a HL7 message and send it on to a local MLLP server.
     // okay this follows the specification of https://hapifhir.github.io/hapi-hl7v2/hapi-hl7overhttp/specification.html
     //  note we cannot parse the HL7 message directly in the @RequestBody because it is not json/xml
     // so hence the String and parsing inside of the function based on HAPI HL7 system.
@@ -112,7 +185,6 @@ public class MedicationController {
        
         return hl7AckMessage.toString() ;
     }
-
 
     // okay this follows the specification of https://hapifhir.github.io/hapi-hl7v2/hapi-hl7overhttp/specification.html
     //  note we cannot parse the HL7 message directly in the @RequestBody because it is not json/xml
@@ -181,6 +253,65 @@ public class MedicationController {
         return outputFHIR ;
     }
 
+        // okay this follows the specification of https://hapifhir.github.io/hapi-hl7v2/hapi-hl7overhttp/specification.html
+    //  note we cannot parse the HL7 message directly in the @RequestBody because it is not json/xml
+    // so hence the String and parsing inside of the function based on HAPI HL7 system.
+    // Note we dont really do anything with the message !
+    @RequestMapping(value="/hl7/**",
+                  headers="Accept=*/*",
+                  method = { RequestMethod.POST },
+                  consumes = {" x-application/hl7-v2+er7"},
+                   produces = { "x-application/hl7-v2+er7" })
+    @ResponseBody
+    public String processHL7Request(
+        @RequestParam Map<String, String> reqParam,
+        @RequestHeader Map<String, String> reqHeaders, 
+        @RequestBody String strHL7,
+        HttpServletRequest request)
+            throws IOException, NameNotFoundException {
+                Message hl7AckMessage=null;
+                Message hl7Message = null;
+
+               // System.out.println("Request: " + strHL7.replace("\n","\r"));
+                
+                try {
+                    PipeParser pipeParser = new PipeParser();
+                    // AF message from HL7 rest API may have the wrong carriage return force it to the correct one or parser fails !
+                    hl7Message = pipeParser.parse(strHL7.replace("\n","\r"));
+   //                 this.kafkaProducer.toString();   
+                    
+
+                    Terser terser = new Terser(hl7Message);
+
+                    HL7TerserHelper terserhelper = new HL7TerserHelper(terser);
+                    try {
+                        String data = terserhelper.getData("/.MSH-6");
+                        System.out.println("Field 6 of MSH, the destination is :" + data);
+                        data = terserhelper.getData("/.MSH-9");
+                        System.out.println("Field 9 of MSH, the destination is :" + data);
+
+                                   // AF this gives us the fifth OBX item (ordered from 0) 5 component and 5th subcomponent 1^2^3^4^5^ !
+                        //    not sure if this message will be hardwired like this in all situations but assuming static 
+                        //    for now.  NB XML FHIR message !
+          
+                        // Just going to place this XML message on the standard kafka queue for now.  This actually makes no sense.
+  //                      this.kafkaProducer.saveString(fhirMsg);
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                    //  was constructing ACK message by hand for a little while before finding this.
+                    hl7AckMessage = hl7Message.generateACK();
+            //        this.kafkaProducer.saveString( hl7Message.toString() );
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+               
+       
+        return hl7AckMessage.toString() ;
+    }
+
 
 
     // okay this follows the specification of https://hapifhir.github.io/hapi-hl7v2/hapi-hl7overhttp/specification.html
@@ -193,7 +324,7 @@ public class MedicationController {
                   consumes = {" x-application/hl7-v2+er7"},
                    produces = { "x-application/hl7-v2+er7" })
     @ResponseBody
-    public String processHL7Request(
+    public String processHL7StrataRequest(
         @RequestParam Map<String, String> reqParam,
         @RequestHeader Map<String, String> reqHeaders, 
         @RequestBody String strHL7,
